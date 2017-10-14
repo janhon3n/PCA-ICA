752a8655dcd15f673d4e8f84831eca4aa3dd14b3
Mikael Janhonen <janhon3n@gmail.com>
2017-10-14 19:15:11 +0300

    Create function to calculate the difference between 2 vectors

--- ica.m
+++ ica.m
@@ -36,3 +36,25 @@ subplot(3,2,5);
 plot(Y(1,:));
 subplot(3,2,6);
 plot(Y(2,:));
+% Calculates the difference between two vectors.
+% The diffenrece is the euclidean distance between the vectors.
+% It is calculated with the formula Sqrt((a1 - b1)^2 + (a2 - b2)^2 + .... + (an - bn)^2)
+%
+% Parameters:
+%   vec1 - first vector
+%   vec2 - second vector
+%
+% Returns:
+% diff - The difference between the vectors
+%
+function [diff] = calculateDifference(vec1, vec2)
+    if length(vec1) ~= length(vec2)
+        error("Vectors must have the same length.");
+    else
+        diff = 0;
+        for i = 1:length(vec1)
+            diff = diff + (vec1(i) - vec2(i))^2;
+        end
+        diff = sqrt(diff);
+    end
+end
